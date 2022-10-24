0a88263e95225bcb0aebd964ede896618c2d63c3
Daniel Hernandez <Daniel.Hernandez@autoweb.com>
2022-10-24 16:26:17 -0600

    Removes unused tests

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -9,34 +9,13 @@ class Results extends Component {
     super(props);
     this.state = {
       titles: {
-        stroop: "Stroop Results",
         trails: "Trails Results",
-        hybrid: "Hybrid Test Results",
       },
       headers: {
-        stroop: [
-          { label: "Event", key: "event" },
-          { label: "Word", key: "word" },
-          { label: "Colour", key: "colour" },
-          { label: "Selected Colour", key: "selected" },
-          { label: "Outcome", key: "outcome" },
-          { label: "Time elapsed", key: "elapsed" },
-          { label: "Reaction time", key: "reaction" },
-        ],
         trails: [
           { label: "Event", key: "event" },
           { label: "Token", key: "token" },
           { label: "Selected Token", key: "selected" },
-          { label: "Outcome", key: "outcome" },
-          { label: "Time elapsed", key: "elapsed" },
-          { label: "Reaction time", key: "reaction" },
-        ],
-        hybrid: [
-          { label: "Event", key: "event" },
-          { label: "Correct Word", key: "correctWord" },
-          { label: "Correct Colour", key: "correctColour" },
-          { label: "Selected Word", key: "selectedWord" },
-          { label: "Selected Colour", key: "selectedColour" },
           { label: "Outcome", key: "outcome" },
           { label: "Time elapsed", key: "elapsed" },
           { label: "Reaction time", key: "reaction" },
@@ -47,40 +26,14 @@ class Results extends Component {
   }
 
   componentDidMount() {
-    if (this.props.results.type === "stroop") {
-      this.stroopEvents(this.props.results.data);
-    } else if (this.props.results.type === "trails") {
+    if (this.props.results.type === "trails") {
       this.trailsEvents(this.props.results.data);
-    } else if (this.props.results.type === "hybrid") {
-      this.hybridEvents(this.props.results.data);
     }
   }
 
   componentWillUnmount() {
     this.setState({ data: null });
   }
-
-  stroopEvents = (results) => {
-    const { events, colours, words, start } = results;
-    let lastTs = start;
-
-    const data = events.map((event, index) => {
-      let obj = {
-        event: index + 1,
-        word: event.word,
-        colour: words[colours.indexOf(event.color)].toUpperCase(),
-        selected: words[colours.indexOf(event.selectedColor)].toUpperCase(),
-        outcome: event.type,
-        elapsed: (event.stamp - start) / 1000,
-        reaction: (event.stamp - lastTs) / 1000,
-      };
-      lastTs = event.stamp;
-
-      return obj;
-    });
-
-    this.setState({ data });
-  };
 
   trailsEvents = (results) => {
     const { events, start } = results;
@@ -103,28 +56,6 @@ class Results extends Component {
     this.setState({ data });
   };
 
-  hybridEvents = (results) => {
-    const { events, start } = results;
-    let lastTs = start;
-
-    const data = events.map((event, index) => {
-      let obj = {
-        event: index + 1,
-        correctWord: event.correctToken.text,
-        correctColour: event.correctToken.coltext,
-        selectedWord: event.selectedToken.text,
-        selectedColour: event.selectedToken.coltext,
-        outcome: event.type,
-        elapsed: (event.stamp - start) / 1000,
-        reaction: (event.stamp - lastTs) / 1000,
-      };
-      lastTs = event.stamp;
-
-      return obj;
-    });
-
-    this.setState({ data });
-  };
 
   render() {
     //const {date } = this.state;
