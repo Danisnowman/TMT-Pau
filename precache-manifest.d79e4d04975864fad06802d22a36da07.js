16fb396826d0f5486ca996bc72cc5ccd06974f1b
Daniel Hernandez <Daniel.Hernandez@autoweb.com>
2022-10-24 15:04:01 -0600

    Updates

---
+++ precache-manifest.d79e4d04975864fad06802d22a36da07.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb849f7047f9f1fc8470a5b23a7cb97",
+    "url": "/TMT-Pau/index.html"
+  },
+  {
+    "revision": "aff57dfbfcaa9fda7d9d",
+    "url": "/TMT-Pau/static/css/2.560b9325.chunk.css"
+  },
+  {
+    "revision": "d4eb920728be217969e1",
+    "url": "/TMT-Pau/static/css/main.1693a569.chunk.css"
+  },
+  {
+    "revision": "aff57dfbfcaa9fda7d9d",
+    "url": "/TMT-Pau/static/js/2.09c25462.chunk.js"
+  },
+  {
+    "revision": "16c35096ce93e17ab19e8c44452ac53a",
+    "url": "/TMT-Pau/static/js/2.09c25462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4eb920728be217969e1",
+    "url": "/TMT-Pau/static/js/main.68d38116.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/TMT-Pau/static/js/main.68d38116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2003cb0ce2b0e1f7acd9",
+    "url": "/TMT-Pau/static/js/runtime-main.32e4279b.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/TMT-Pau/precache-manifest.d79e4d04975864fad06802d22a36da07.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TMT-Pau/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
